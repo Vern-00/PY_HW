b5570db36fd67d36cd9fc7c64839f9d60d844686
Vern Toor <verntoor23@gmail.com>
2025-10-14 23:55:06 -0700

    spacing fix?

--- bug1.py
+++ bug1.py
@@ -14,18 +14,18 @@ class Circle(Base):
     def draw(self):
         return f"""
 ({self.x}, {self.y})\n{self.size}
-        , - ~ ~ ~ - ,
-    , '               ' ,
-  ,                      ,
- ,                        ,
-,                          ,
-,                          ,
-,                          ,
- ,                        ,
-  ,                      ,
-    ,                 , '
-      ' - , _ _ _ , '
-              """
+, - ~ ~ ~ - ,
+, ' ' ,
+, ,
+, ,
+, ,
+, ,
+, ,
+, ,
+, ,
+, , '
+' - , _ _ _ , '
+"""
 
 def main():
     c = Circle(1,2,3)

cef898ec00edf17890d431caed3a5626cc8e16cb
Vern Toor <verntoor23@gmail.com>
2025-10-14 23:10:37 -0700

    merge V2

--- merge.py
+++ merge.py
@@ -5,12 +5,12 @@ def merge_list(a,b):
     b.sort()
     i = j = 0
     out = []
-    while i < len(a) and j < len (b):
+    while i < len(a) and j < len(b):
         if a[i] <= b[j]:
             out.append(a[i])
-            i++
+            i += 1
         else:
             out.append(b[j])
-            j++
+            j += 1
     out.extend(a[i:]); out.extend(b[j:])
     return out
